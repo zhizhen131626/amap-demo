2cc898efadd04043ec66d5adcfec0adbaab35b52
huangzhizhen <huangzhizhen@comtom.cn>
2023-11-20 09:51:44 +0800

    feat: 创建项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    111
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
